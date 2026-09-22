fb3bf29f65b427ae87ec63ffd62ab055cf9290c9
Yoooga0824 <17582495726@163.com>
2026-09-22 17:28:25 +0800

    chore(reviews): expand certificate date field hint

--- app/pages/reviews.vue
+++ app/pages/reviews.vue
@@ -655,7 +655,7 @@ watch(formKind, () => resetForm());
             <UFormField label="获奖时间" name="date" required>
               <UInput v-model="form.date" class="w-full" type="date" />
             </UFormField>
-            <UFormField label="证书时间（可选）" name="certificateDate">
+            <UFormField label="证书时间（可选，若填则需同时上传证书，若暂无可日后补充提交）" name="certificateDate">
               <UInput v-model="form.certificateDate" class="w-full" type="date" />
             </UFormField>
             <UFormField label="成员排序（输入学号后点击回车保存）" name="members">
